ed701478e5e8ddcf0fde1d675ae20b0cfc376709
Kevin Scharpf <kevin.scharpf@gmail.com>
2013-09-27 19:29:17 -0400

    fixed but doesn't work for some primes - aka 15

--- factors.hs
+++ factors.hs
@@ -5,39 +5,46 @@ minus (x:xs) (y:ys) = case (compare x y) of
            GT ->     minus (x:xs) ys
 minus xs _ = xs
 
-maxPossible = 775147
-
-numberToFactor :: Int
-numberToFactor = maxPossible
-
-
-possiblePrimeList :: Int -> [Int]
+possiblePrimeList :: Integer -> [Integer]
 possiblePrimeList x = [2..x]
 
---primeSieve :: [Integer] -> [Integer] -> [Integer]
---primeSieve [] primeList = primeList
---primeSieve uncheckedList primeList = primeSieve (filter (\x -> (mod x (head uncheckedList)) /= 0) (tail uncheckedList)) (primeList ++ [(head uncheckedList)])
-primesToQ :: Int -> [Int]
+primesToQ :: Integer -> [Integer]
 primesToQ m = 2 : sieve [3,5..m] where
     sieve [] = []
     sieve (p:xs) = p : sieve (xs `minus ` [p*p, p*p+2*p..m])
---primeSieve (p:xs)
 
---possiblePrimes :: Integer -> [Integer]
---possiblePrimes x = primeSieve (possiblePrimeList x) []
 
-defaultPossiblePrimes :: [Int]
---defaultPossiblePrimes = possiblePrimes maxPrime
-defaultPossiblePrimes = primesToQ numberToFactor
+defaultPossiblePrimes :: Integer -> [Integer]
+defaultPossiblePrimes numberToFactor = primesToQ numberToFactor
 
-primeFactors :: [Int] -> Int -> [Int] -> [Int]
+primeFactors :: [Integer] -> Integer -> [Integer] -> [Integer]
 primeFactors [] n ys = ys
 primeFactors (x:xs) n ys
-    | (x*x) > n = ys
-    | (mod n x) == 0 = (primeFactors (x:xs) (floor n/x) (ys ++ x))
+    | (mod n x) == 0 = (primeFactors (x:xs) (n `div` x) (ys ++ [x]))
     | otherwise = primeFactors xs n ys
 
-maxPrimes = maximum defaultPossiblePrimes
+(^!) :: Num a => a -> Int -> a
+(^!) x n = x^n
+ 
+squareRoot :: Integer -> Integer
+squareRoot 0 = 0
+squareRoot 1 = 1
+squareRoot n =
+   let twopows = iterate (^!2) 2
+       (lowerRoot, lowerN) =
+          last $ takeWhile ((n>=) . snd) $ zip (1:twopows) twopows
+       newtonStep x = div (x + div n x) 2
+       iters = iterate newtonStep (squareRoot (div n lowerN) * lowerRoot)
+       isRoot r  =  r^!2 <= n && n < (r+1)^!2
+   in  head $ dropWhile (not . isRoot) iters
 
-main :: IO()
-main = putStrLn (show maxPrimes)
+
+--showFactors :: Integer -> [Integer]
+--showFactors n = primeFactors (defaultPossiblePrimes $ (squareRoot n)) n []
+
+--showFactorsS :: String -> String
+--showFactorsS v = show (showFactors (read v::Integer))
+
+--main :: IO()
+--main = interact showFactorsS
+main = putStrLn (show (primeFactors (defaultPossiblePrimes $ squareRoot 600851475143) 600851475143 []))
